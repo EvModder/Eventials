3a45f477b57cbbacb204bd5be69dd631079b73b0
EvModder <evmodder@gmail.com>
2026-01-15 18:58:56 -0600

    1.21.1

--- src/Eventials/listeners/PlayerLoginListener.java
+++ src/Eventials/listeners/PlayerLoginListener.java
@@ -110,7 +110,7 @@ public class PlayerLoginListener implements Listener{
 		final OfflinePlayer joiner = plugin.getServer().getOfflinePlayer(uuid);
 		final boolean useName = !joiner.isOnline() || !joiner.getPlayer().isValid();
 		final String joinMsg = new TranslationComponent("multiplayer.player.joined",
-				new Component[]{useName ? new RawTextComponent(joiner.getName()) : new SelectorComponent(uuid)},
+				new Component[]{useName ? new RawTextComponent(""+joiner.getName()) : new SelectorComponent(uuid)},
 				/*insert=*/null, /*click=*/null, /*hover=*/null, /*color=*/"yellow", /*formats=*/null).toString();
 		for(Player p : plugin.getServer().getOnlinePlayers()){
 			if(!p.getUniqueId().equals(uuid)){
--- src/Eventials/listeners/PlayerSleepListener.java
+++ src/Eventials/listeners/PlayerSleepListener.java
@@ -112,7 +112,7 @@ public class PlayerSleepListener implements Listener{
 		int numToSkipThunder = (int)(Math.ceil(needToSkipThunder) > needToSkipThunder ? Math.ceil(needToSkipThunder)
 				: needToSkipThunder + (!PERCENT_INCLUSIVE && needToSkipThunder < MAX_SLEEPERS ? 1 : 0));
 		//pl.getLogger().info("cur_sleepers: "+CUR_SLEEPERS+", max_sleepers: "+MAX_SLEEPERS+", num_to_skip_night: "+numToSkipNight);
-		if(SKIP_IF_DAYLIGHT_CYCLE_IS_OFF || world.getGameRuleValue(GameRule.DO_DAYLIGHT_CYCLE)){
+		if(SKIP_IF_DAYLIGHT_CYCLE_IS_OFF || world.getGameRuleValue(GameRule.ADVANCE_TIME)){
 			if(includeTrigger){
 				Player player = pl.getServer().getPlayer(triggerPlayer);
 				if(player != null){
